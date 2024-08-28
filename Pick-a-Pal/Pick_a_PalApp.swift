f7a4390a098530fc7d6b7f4227a2b588a501da51
Woo Sung Jahng <jlittle0919@icloud.com>
2024-08-28 19:01:53 +0900

    Initial commit

---
+++ Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Woo Sung Jahng on 2024/04/18.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
